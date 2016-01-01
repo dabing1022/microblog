b425f0dfa765a3225b688188d971fa3934380169
ChildhoodAndy <zhaoweibing@didapinche.com>
2016-01-01 16:56:20 +0800

    more wtf feature

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,18 @@
 from flask.ext.wtf import Form
 from flask_wtf import Form, RecaptchaField
-from wtforms import StringField, BooleanField, TextField
+from flask_wtf.html5 import IntegerField, TelField, TelInput
+from wtforms import StringField, BooleanField, TextField, validators
 from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired, url
-
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
-
 class LinkForm(Form):
     url = URLField(validators=[url()])
+    phone = IntegerField('mobilephone', [validators.NumberRange(min = 0, max = 11)])
+
+class SignupForm(Form):
+    username = TextField('Username')
+    recaptcha = RecaptchaField()
